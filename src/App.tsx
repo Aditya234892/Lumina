62c8779f7126a4e2a8598c8fd08b56f2d982c79c
Aditya <adityadevansh2002@gmail.com>
2025-01-24 14:55:30 +0530

    Initialised the project using Vite with Typescript, React js and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-[100vw] h-[100vh] bg-gray-800'>
+
+    </div>
+  )
+}
+
+export default App
